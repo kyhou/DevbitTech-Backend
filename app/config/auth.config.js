445dd649b699bc852c8d1aee6a074e3d81160169
kyhou <joelskill@gmail.com>
2024-08-04 20:14:08 -0300

    Busca o secret também de uma variável de ambiente.

--- app/config/auth.config.js
+++ app/config/auth.config.js
@@ -1,8 +1,16 @@
 const fs = require('fs');
 const path = require('path')
 
+let secret;
+
+if (fs.existsSync(path.join(__dirname, 'hs256.key'))) {
+
+    secret = fs.readFileSync(path.join(__dirname, 'hs256.key'), 'utf8');
+} else {
+    secret = process.env.SECRET;
+}
 module.exports = {
-    secret: fs.readFileSync(path.join(__dirname, 'hs256.key'), 'utf8'),
+    secret,
     jwtExpiration: 3600,           // 1 hour
     jwtRefreshExpiration: 86400,   // 24 hours
 };
