1e401f7e0e5def60689dcae0305843915f8be594
kyhou <joelskill@gmail.com>
2024-08-03 23:19:28 -0300

    Altera configurações do postgres para compatibilidade com a vercel.

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -2,8 +2,7 @@ module.exports = {
     HOST: process.env.POSTGRES_HOST,
     USER: process.env.POSTGRES_USER,
     PASSWORD: process.env.POSTGRES_PASSWORD,
-    PORT: Number(process.env.POSTGRES_PORT),
-    DB: process.env.POSTGRES_DB,
+    DB: process.env.POSTGRES_DATABASE,
     dialect: "postgres",
     pool: {
       max: 5,
--- app/models/index.js
+++ app/models/index.js
@@ -3,10 +3,9 @@ const dbConfig = require("../config/db.config.js");
 const Sequelize = require("sequelize");
 const sequelize = new Sequelize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD, {
   host: dbConfig.HOST,
-  port: dbConfig.PORT,
   dialect: dbConfig.dialect,
   operatorsAliases: 0,
-  //logging: console.log,
+  logging: console.log,
 
   pool: {
     max: dbConfig.pool.max,
