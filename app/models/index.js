61206e4688937e4e3c3edd2c8b409415f8c21f85
kyhou <joelskill@gmail.com>
2024-08-04 02:09:16 -0300

    Adiciona opção "dialectModule: require('pg')" ao criar a instancia do Sequelize.

--- app/models/index.js
+++ app/models/index.js
@@ -6,6 +6,7 @@ const sequelize = new Sequelize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD, {
   dialect: dbConfig.dialect,
   operatorsAliases: 0,
   logging: console.log,
+  dialectModule: require('pg'),
 
   pool: {
     max: dbConfig.pool.max,
