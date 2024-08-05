c8877d3846fa391c1158559070a6a639699785d8
kyhou <joelskill@gmail.com>
2024-08-04 22:39:00 -0300

    Altera server.js e vercel.json e adiciona api/index.js para trabalhar com a vercel.

--- server.js
+++ server.js
@@ -54,10 +54,10 @@ glob.sync('./app/routes/**/*.js').forEach(function (file) {
 });
 
 
-// set port, listen for requests
-const PORT = process.env.PORT || 8088;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}.`);
-});
+// // set port, listen for requests
+// const PORT = process.env.PORT || 8088;
+// app.listen(PORT, () => {
+//   console.log(`Server is running on port ${PORT}.`);
+// });
 
 module.exports = app;
